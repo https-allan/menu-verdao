0b288c61ee8d8d4ce27df51fa7061e7abb07ad36
https-allan <code.allanjose@gmail.com>
2024-07-24 07:50:52 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-const menuToggle = document.getElementByI("menu-toggle");
-const menu = document.getElementById("menu");
-const closeMenu = document.getElementById("close-menu");
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.getElementById("menu");
+  const closeMenuButton = document.getElementById("close-menu");
+  const menuToggle = document.getElementById("menu-toggle");
 
-menuToggle.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-});
+  menuToggle.addEventListener("click", () => {
+    menu.classList.remove("translate-x-full");
+    menu.classList.remove("hidden");
+  });
 
-closeMenu.addEventListener("click", () => {
-  menu.classList.add("hidden");
+  closeMenuButton.addEventListener("click", () => {
+    setTimeout(() => {
+      menu.classList.add("hidden");
+    }, 300);
+  });
 });
